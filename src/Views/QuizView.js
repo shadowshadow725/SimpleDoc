08428a40853072e952b77909cb118aaee0cf03c7
Trevor Nagy <1430911+trevornagy@users.noreply.github.com>
2020-10-06 13:25:56 -0400

    Revert "Cleaned up code before pull request. Will work on UI after discussion with team."
    This reverts commit 0c77769cf11197bb822e51d6f9a7270de0064596.

--- src/Views/QuizView.js
+++ src/Views/QuizView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Stepper, Step, StepLabel } from "@material-ui/core";
 
 import MultipleChoiceQuestion from "../Components/MultipleChoiceQuestion";
 import SimpleDocRest from "../api/SimpleDocRest";
@@ -14,6 +15,7 @@ class QuizView extends React.Component {
   fetchQuiz = async () => {
     SimpleDocRest.get(`/quizzes/1/`)
       .then((result) => {
+        console.log(result);
         this.setState({
           quizTitle: result.data.title,
           quizQuestions: result.data.quizquestion_set,
@@ -31,7 +33,7 @@ class QuizView extends React.Component {
   render() {
     const { quizQuestions, quizId } = this.state;
     return (
-      <div>
+      <div key={quizId}>
         {quizQuestions.map(({ question, quizoption_set, id }) => (
           <MultipleChoiceQuestion
             key={id}
